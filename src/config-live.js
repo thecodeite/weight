7fd963f0a61e059af6c274c9f53bef691e89d40c
Sam Plews <the@codeite.com>
2017-05-02 12:42:05 +0100

    working

--- src/config-live.js
+++
@@ -1,6 +0,0 @@
-global.config = {
-  apiPrefix: 'https://api.freeagent.com.auth.codeite.net/v2',
-  apiDomain: 'api.freeagent.com',
-  loginUrl: 'https://auth.codeite.net/login',
-  authorizeUrl: 'http://auth.codeite.net/authorize'
-}
--- src/config-local.js
+++
@@ -1,6 +0,0 @@
-global.config = {
-  apiPrefix: 'http://api.sandbox.freeagent.com.auth.codeite.dev/v2',
-  apiDomain: 'api.sandbox.freeagent.com',
-  loginUrl: 'http://auth.codeite.dev/login',
-  authorizeUrl: 'http://auth.codeite.dev/authorize'
-}
